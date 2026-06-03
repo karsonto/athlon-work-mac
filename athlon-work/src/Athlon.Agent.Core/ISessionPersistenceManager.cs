e97641cd689689e9c21c83a76f183dc92cb3c89a
unknown <karsontao@hotmail.com>
2026-06-03 22:43:43 +0800

    chore: remove obsolete WPF/C# project files (migrated to macOS/Swift)

--- athlon-work/src/Athlon.Agent.Core/ISessionPersistenceManager.cs
+++
@@ -1,7 +0,0 @@
-namespace Athlon.Agent.Core;
-
-public interface ISessionPersistenceManager
-{
-    Task PersistMessageAsync(AgentSession session, ChatMessage message, CancellationToken cancellationToken, bool saveFull = false);
-    Task SaveSessionAsync(AgentSession session, CancellationToken cancellationToken);
-}
