696e87fc0ece59582ab912485aaa1686dfdef529
Karson To <karsontao@hotmail.com>
2026-07-29 13:42:48 +0800

    Initial Commit

---
+++ Athlon-work/Athlon_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  Athlon_workApp.swift
+//  Athlon-work
+//
+//  Created by karson on 2026/7/29.
+//
+
+import SwiftUI
+
+@main
+struct Athlon_workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athlon-workTests/Athlon_workTests.swift
@@ -0,0 +1,17 @@
+//
+//  Athlon_workTests.swift
+//  Athlon-workTests
+//
+//  Created by karson on 2026/7/29.
+//
+
+import Testing
+@testable import Athlon_work
+
+struct Athlon_workTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
